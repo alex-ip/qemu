8d678e566846a65388163b6d0ddb9f99b9fb92f2
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2022-05-07 01:09:28 +0100

    lasips2: move QOM definitions from lasips2.c to lasips2.h
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- include/hw/input/lasips2.h
+++ include/hw/input/lasips2.h
@@ -9,7 +9,49 @@
 
 #include "exec/hwaddr.h"
 
+typedef struct LASIPS2State LASIPS2State;
+
+struct LASIPS2Port {
+    SysBusDevice parent_obj;
+
+    LASIPS2State *parent;
+    MemoryRegion reg;
+    void *dev;
+    uint8_t id;
+    uint8_t control;
+    uint8_t buf;
+    bool loopback_rbne;
+    bool irq;
+};
+
+#define TYPE_LASIPS2_PORT "lasips2-port"
+OBJECT_DECLARE_SIMPLE_TYPE(LASIPS2Port, LASIPS2_PORT)
+
+struct LASIPS2KbdPort {
+    LASIPS2Port parent_obj;
+};
+
+#define TYPE_LASIPS2_KBD_PORT "lasips2-kbd-port"
+OBJECT_DECLARE_SIMPLE_TYPE(LASIPS2KbdPort, LASIPS2_KBD_PORT)
+
+struct LASIPS2MousePort {
+    LASIPS2Port parent_obj;
+};
+
+#define TYPE_LASIPS2_MOUSE_PORT "lasips2-mouse-port"
+OBJECT_DECLARE_SIMPLE_TYPE(LASIPS2MousePort, LASIPS2_MOUSE_PORT)
+
+struct LASIPS2State {
+    SysBusDevice parent_obj;
+
+    MemoryRegion mem;
+    LASIPS2Port kbd;
+    LASIPS2Port mouse;
+    qemu_irq irq;
+};
+
 #define TYPE_LASIPS2 "lasips2"
+OBJECT_DECLARE_SIMPLE_TYPE(LASIPS2State, LASIPS2)
 
 void lasips2_initfn(MemoryRegion *address_space, hwaddr base, qemu_irq irq);
 
