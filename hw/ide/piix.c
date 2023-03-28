7d209b0d3704eba4c8b639afe07a955c89e549a7
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2023-03-28 20:00:56 +0100

    ide/piix.c: destory legacy ioports during PCIDevice exit
    Use the new pci_ide_unregister_legacy_ioports() function to destory the legacy
    ioports when the device is finalized.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- hw/ide/piix.c
+++ hw/ide/piix.c
@@ -171,6 +171,7 @@ static void pci_piix_ide_exitfn(PCIDevice *dev)
     for (i = 0; i < 2; ++i) {
         memory_region_del_subregion(&d->bmdma_bar, &d->bmdma[i].extra_io);
         memory_region_del_subregion(&d->bmdma_bar, &d->bmdma[i].addr_ioport);
+        pci_ide_unregister_legacy_ioports(d, &d->bus[i]);
     }
 }
 
