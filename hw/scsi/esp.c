535cfa63c28df2e3a68fa9d04696e75f4661dfee
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2023-08-14 13:15:25 +0100

    esp.c: convert do_dma_pdma_cb() do_cmd path to check for SCSI phase instead
    Currently do_cmd is used to determine whether MESSAGE OUT and COMMAND phase data
    is being accumulated in cmdfifo. Update esp_do_dma() to check directly for these
    two SCSI phases instead.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- hw/scsi/esp.c
+++ hw/scsi/esp.c
@@ -517,7 +517,9 @@ static void do_dma_pdma_cb(ESPState *s)
     int len;
     uint32_t n;
 
-    if (s->do_cmd) {
+    switch (esp_get_phase(s)) {
+    case STAT_MO:
+    case STAT_CD:
         /* Copy FIFO into cmdfifo */
         n = esp_fifo_pop_buf(&s->fifo, buf, fifo8_num_used(&s->fifo));
         n = MIN(fifo8_num_free(&s->cmdfifo), n);
@@ -549,10 +551,8 @@ static void do_dma_pdma_cb(ESPState *s)
             s->rregs[ESP_RINTR] |= INTR_BS;
             esp_raise_irq(s);
         }
-        return;
-    }
+        break;
 
-    switch (esp_get_phase(s)) {
     case STAT_DO:
         if (!s->current_req) {
             return;
